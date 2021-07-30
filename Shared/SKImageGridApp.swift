cfc78c72582c3cef5e466bb489dd803a15afeeba
Brett Chapin <brett.chapin89@gmail.com>
2021-07-30 11:46:08 -0400

    Initial Commit

---
+++ Shared/SKImageGridApp.swift
@@ -0,0 +1,17 @@
+//
+//  SKImageGridApp.swift
+//  Shared
+//
+//  Created by Brett Chapin on 7/30/21.
+//
+
+import SwiftUI
+
+@main
+struct SKImageGridApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
